e4967d5287c7b205baaf7b8e5e58cc9990d7df75
Vadym Bondarenko <vadem.bondarenko@gmail.com>
2022-10-14 15:48:30 +0300

    hw 10

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(onInputData, DEBOUNCE_DELAY));
+
+function onInputData(evt) {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+  const searchInput = evt.target.value.trim();
+  if (searchInput !== '') {
+    fetchCountries(searchInput).then(renderCountry).catch(error);
+  }
+}
+function renderCountry(data) {
+  if (data.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (data.length >= 2 && data.length <= 10) {
+    createMarkUpCountryList(data);
+  } else if (data.length === 1) {
+    createMarkUpCountryInfo(data);
+  } else if (data.length === 0) {
+    error(err);
+  }
+  console.log(data);
+}
+
+function createMarkUpCountryList(obj) {
+  const markup = obj
+    .map(item => {
+      return `<li class="item">
+      <img src="${item.flags.png}" alt="flag" width=30px; height=20px>
+   <h2 class='title'>${item.name.common}</h2>
+    </li>`;
+    })
+    .join('');
+
+  countryList.innerHTML = markup;
+}
+function createMarkUpCountryInfo(obj) {
+  const markup = obj
+    .map(item => {
+      return `<div class="header">
+           <img src="${item.flags.png}" alt="flag" width=30px; height=20px>
+   <h2 class='title'> ${item.name.common}</h2>
+      </div>
+   
+   <p><b>Capital</b>: ${item.capital}</p>
+   <p><b>Population</b>: ${item.population}</p>
+       <p><b>Languages</b>: ${Object.values(item.languages)} </p>
+    `;
+    })
+    .join('');
+
+  countryInfo.innerHTML = markup;
+}
+function error(err) {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
